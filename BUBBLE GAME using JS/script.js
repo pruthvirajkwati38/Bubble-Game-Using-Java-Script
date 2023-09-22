ef476573161e04a3e26f22439e41300bbf1e96a2
pruthvirajkwati38 <142924903+pruthvirajkwati38@users.noreply.github.com>
2023-09-22 19:00:17 +0530

    Add files via upload

---
+++ BUBBLE GAME using JS/script.js
@@ -0,0 +1,54 @@
+
+var timer =60;
+var score=0;
+var hitrn=0;
+
+//jispe click karoge vo element par event raise hoga, aur event listener naa milne par event element ke parent par listener dhundhega, waha bi naa milne par event parent ke parent ke parent par listener dhundega.
+
+function increaseScore(){
+    score += 10;
+    document.querySelector("#scoreval").textContent = score;
+}
+
+function getNewHit(){
+    hitrn = Math.floor(Math.random()*10);
+    document.querySelector("#hitval").textContent=hitrn;
+
+}
+
+function makeBubble(){
+var clutter =" ";
+
+for(var i=1; i<71; i++){
+    var rn =Math.floor(Math.random()*10)
+    clutter +=`<div class="bubble">${rn}</div>`;
+}
+
+document.querySelector("#pbtm").innerHTML=clutter;
+}
+function runTimer(){
+    var timerint=setInterval(function(){
+        if(timer>0){
+        timer--;
+        document.querySelector("#timerval").textContent=timer;
+        }
+        else{
+            clearInterval(timerint);
+            document.querySelector("#pbtm").innerHTML = `<h1>Game Over</h1>`
+        }
+    },1000);
+}
+
+document.querySelector("#pbtm").addEventListener("click",function (dets){
+    var clickednum=Number(dets.target.textContent);
+    if(clickednum ===hitrn){
+        increaseScore();
+        makeBubble();
+        getNewHit();
+    }
+    
+});
+
+runTimer();
+makeBubble();
+getNewHit();
